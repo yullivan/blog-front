9fe3f543c51db74fe4ced6ce5d4dbff71c2942bd
Yul Jeon <dalbitstream@gmail.com>
2025-03-17 19:20:58 +0900

    feat: '메인 페이지'에 '글 목록 페이지'로 이동하는 링크 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <div>📄 홈 페이지</div>
+      <Link href="/posts">글 목록으로 이동</Link>
+    </div>
+  );
 }
