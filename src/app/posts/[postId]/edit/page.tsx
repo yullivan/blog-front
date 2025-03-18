575b0fb988957ac63ec4afda25495f772f08ef8a
Yul Jeon <dalbitstream@gmail.com>
2025-03-18 11:18:16 +0900

    feat: 글 수정 폼 추가

--- src/app/posts/[postId]/edit/page.tsx
+++ src/app/posts/[postId]/edit/page.tsx
@@ -1,12 +1,23 @@
+import BackwardButton from "@/components/backward-button";
+
 export default async function EditPost({
   params,
 }: {
   params: Promise<{ postId: string }>;
 }) {
   const { postId } = await params;
+
   return (
     <div>
       <div>📄 글 수정 페이지 (ID: {postId})</div>
+      <form>
+        <input type="text" name="title" defaultValue={"기존 글 제목"} />
+        <textarea name="content" defaultValue={"기존 글 내용"}></textarea>
+        <div>
+          <BackwardButton />
+          <button type="submit">수정하기</button>
+        </div>
+      </form>
     </div>
   );
 }
