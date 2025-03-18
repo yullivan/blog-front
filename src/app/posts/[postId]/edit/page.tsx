1c4392b01ea5456f704406a1464549d0cc527cf9
Yul Jeon <dalbitstream@gmail.com>
2025-03-18 11:31:32 +0900

    feat: 서버 액션을 만들어 사용자가 입력한 수정 데이터 처리

--- src/app/posts/[postId]/edit/page.tsx
+++ src/app/posts/[postId]/edit/page.tsx
@@ -1,4 +1,5 @@
 import BackwardButton from "@/components/backward-button";
+import { redirect } from "next/navigation";
 
 export default async function EditPost({
   params,
@@ -7,10 +8,26 @@ export default async function EditPost({
 }) {
   const { postId } = await params;
 
+  async function editPost(formData: FormData) {
+    "use server";
+
+    const postId = formData.get("postId");
+    const rawFormData = {
+      title: formData.get("title"),
+      content: formData.get("content"),
+    };
+
+    // TODO: API 서버로 수정 요청 보내기
+    console.log("글 수정: " + postId);
+
+    redirect(`/posts/${postId}`);
+  }
+
   return (
     <div>
       <div>📄 글 수정 페이지 (ID: {postId})</div>
-      <form>
+      <form action={editPost}>
+        <input hidden={true} name="postId" defaultValue={postId} />
         <input type="text" name="title" defaultValue={"기존 글 제목"} />
         <textarea name="content" defaultValue={"기존 글 내용"}></textarea>
         <div>
