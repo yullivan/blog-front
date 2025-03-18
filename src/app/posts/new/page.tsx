1dbd6f62f4cfe93aea11332d1d083e27ac176bd1
Yul Jeon <dalbitstream@gmail.com>
2025-03-18 10:17:57 +0900

    feat: 제목, 내용 입력 폼 만들기

--- src/app/posts/new/page.tsx
+++ src/app/posts/new/page.tsx
@@ -2,6 +2,11 @@ export default function NewPost() {
   return (
     <div>
       <div>📄 새 글 작성 페이지</div>
+      <form>
+        <input type="text" placeholder="제목을 작성하세요" />
+        <textarea placeholder="내용을 작성하세요"></textarea>
+        <button type="submit">작성하기</button>
+      </form>
     </div>
   );
 }
