299557650d3fac36a8762b54ba94f9264b1868fe
Yul Jeon <dalbitstream@gmail.com>
2025-03-18 10:11:04 +0900

    feat: 새 글 작성 버튼 링크 추가

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -7,6 +7,11 @@ export default async function Posts() {
     <div>
       <div>📄 글 목록 페이지</div>
       <Link href="/">홈 페이지로 이동</Link>
+      {/* 새 글 작성 버튼을 위해 <button> 태그를 사용하면
+          클릭 이벤트 리스너를 사용해야 하고
+          그럼 브라우저에서만 동작 가능하기 때문에
+          클라이언트 컴포넌트여야 함 */}
+      <Link href="/posts/new">새 글 작성</Link>
       <ul>
         {posts.map((post) => (
           <li key={post.id}>
