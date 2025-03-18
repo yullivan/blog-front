4fbad39f1a84886603dc9699479c7ca928b78180
Yul Jeon <dalbitstream@gmail.com>
2025-03-18 10:53:04 +0900

    feat: 글 삭제 버튼 추가

--- src/app/posts/[postId]/page.tsx
+++ src/app/posts/[postId]/page.tsx
@@ -1,12 +1,30 @@
+import { redirect } from "next/navigation";
+
 export default async function Post({
   params,
 }: {
   params: Promise<{ postId: string }>;
 }) {
   const { postId } = await params;
+
+  async function deletePost(formData: FormData) {
+    "use server";
+
+    const postId = formData.get("postId");
+
+    // TODO: API 서버로 삭제 요청 보내기
+    console.log("글 삭제");
+
+    redirect("/posts");
+  }
+
   return (
     <div>
       <div>📄 글 상세 페이지 (ID: {postId})</div>
+      <form action={deletePost}>
+        <input hidden={true} name="postId" defaultValue={postId}></input>
+        <button>삭제</button>
+      </form>
     </div>
   );
 }
