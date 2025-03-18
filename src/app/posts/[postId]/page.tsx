e1cbb3e47a7d4fe29217a9bc482e7f96b55cdc92
Yul Jeon <dalbitstream@gmail.com>
2025-03-18 10:46:30 +0900

    feat: URL의 글 ID 값을 글 상세 페이지 컴포넌트에서 다루기

--- src/app/posts/[postId]/page.tsx
+++ src/app/posts/[postId]/page.tsx
@@ -1,7 +1,12 @@
-export default function Post() {
+export default async function Post({
+  params,
+}: {
+  params: Promise<{ postId: string }>;
+}) {
+  const { postId } = await params;
   return (
     <div>
-      <div>📄 글 상세 페이지</div>
+      <div>📄 글 상세 페이지 (ID: {postId})</div>
     </div>
   );
 }
